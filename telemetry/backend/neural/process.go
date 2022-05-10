6c054656ee42609786b453cd8414176bfc8bfcd3
Martin Zimandl <martin.zimandl@gmail.com>
2022-05-10 16:29:11 +0200

    different probability approach

--- telemetry/backend/neural/process.go
+++ telemetry/backend/neural/process.go
@@ -11,24 +11,35 @@ import (
 	"math"
 )
 
+type ActionBin struct {
+	totalCount  int
+	actionCount int
+}
+
 func calculateEntropy(interactions []*NormalizedInteraction, actionName string) (entropy float64) {
-	totalCount := 0
-	timeCount := make(map[int]int)
+	timeBins := make(map[int]*ActionBin)
 	for _, interaction := range interactions {
 		for _, action := range interaction.Actions {
+			timeIndex := int(action.RelativeTime * 10)
+			bin, ok := timeBins[timeIndex]
+			if !ok {
+				bin = &ActionBin{totalCount: 0, actionCount: 0}
+				timeBins[timeIndex] = bin
+			}
+			bin.totalCount++
 			if action.ActionName == actionName {
-				timeCount[int(action.RelativeTime*10)]++
-				totalCount++
+				bin.actionCount++
 			}
 		}
 	}
 
-	for _, count := range timeCount {
-		p := float64(count) / float64(totalCount)
-		entropy -= p * math.Log(p)
+	for _, bin := range timeBins {
+		if bin.actionCount > 0 {
+			p := float64(bin.actionCount) / float64(bin.totalCount)
+			entropy -= p * math.Log(p)
+		}
 	}
+
 	log.Printf("DEBUG: `%s` entropy = %f", actionName, entropy)
-	log.Printf("\tCorresponding uniform entropy = %f", -math.Log(1/float64(totalCount)))
-	log.Printf("\tCorresponding uniform entropy per interaction = %f", -math.Log(1/(float64(totalCount)/float64(len(interactions)))))
 	return
 }

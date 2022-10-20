702d77c115d6be0d93968be05bd64e83deae5641
Tomas Machalek <tomas.machalek@gmail.com>
2022-10-20 16:30:45 +0200

    Add missing assc files

---
+++ services/req.go
@@ -0,0 +1,36 @@
+// Copyright 2022 Tomas Machalek <tomas.machalek@gmail.com>
+// Copyright 2022 Martin Zimandl <martin.zimandl@gmail.com>
+// Copyright 2022 Charles University - Faculty of Arts,
+//                Institute of the Czech National Corpus
+// All rights reserved.
+
+package services
+
+import (
+	"crypto/tls"
+	"io/ioutil"
+	"net/http"
+)
+
+func GetRequest(url, userAgent string) (string, error) {
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: transport}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("User-Agent", userAgent)
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	sbody := string(body)
+	return sbody, nil
+}

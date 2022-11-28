f3405a14419adb9867591ca1a817b19a9df08b39
Tomas Machalek <tomas@machalek.xyz>
2022-11-28 17:06:54 +0100

    Remove 'storage' conf, rename tables...
    ... so we can have them stored within main CNC database

--- cncdb/rdelay/common.go
+++ cncdb/rdelay/common.go
@@ -8,28 +8,7 @@ package rdelay
 
 import (
 	"encoding/json"
-	"fmt"
 )
-
-type Conf struct {
-	Host     string `json:"host"`
-	User     string `json:"user"`
-	Password string `json:"password"`
-	Database string `json:"database"`
-}
-
-func (c *Conf) Validate(context string) error {
-	if c.Host == "" {
-		return fmt.Errorf("%s.host is empty/missing", context)
-	}
-	if c.User == "" {
-		return fmt.Errorf("%s.user is empty/missing", context)
-	}
-	if c.Database == "" {
-		return fmt.Errorf("%s.database is empty/missing", context)
-	}
-	return nil
-}
 
 type DataCleanupResult struct {
 	NumDeletedStats     int64

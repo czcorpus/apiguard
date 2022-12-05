czcorpus/apiguard

commit 65504c46282c4462ca65e80ae6766eb0b1e6290e

Email header issues ... (vs. amavis)

alarms/mail/mail.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"net/smtp"
 	"strings"
+	"time"
 )
 
 // SendNotification sends a general e-mail notification based on
@@ -19,6 +20,7 @@ import (
 // part of the message.
 func SendNotification(
 	client *smtp.Client,
+	location *time.Location,
 	sender string,
 	receivers []string,
 	subject string,
@@ -41,6 +43,7 @@ func SendNotification(
 	headers["Subject"] = subject
 	headers["MIME-Version"] = "1.0"
 	headers["Content-Type"] = "text/html; charset=UTF-8"
+	headers["Date"] = time.Now().In(location).Format("Mon, Jan 02 2006 15:04:05 -0700")
 
 	body := ""
 	for k, v := range headers {

alarms/alarms.go
@@ -121,6 +121,7 @@ func (aticker *AlarmTicker) checkService(entry *serviceEntry, name string, unixT
 						page := aticker.createConfirmationPageURL(newReport, recipient)
 						err := mail.SendNotification(
 							client,
+							aticker.location,
 							aticker.alarmConf.Sender,
 							[]string{recipient},
 							fmt.Sprintf(

commit 86b593fa29df753c4df948ce1eacad42ab856151

Fix date format (caused by 65504c46282c4462ca65e80)

alarms/mail/mail.go
@@ -43,7 +43,7 @@ func SendNotification(
 	headers["Subject"] = subject
 	headers["MIME-Version"] = "1.0"
 	headers["Content-Type"] = "text/html; charset=UTF-8"
-	headers["Date"] = time.Now().In(location).Format("Mon, Jan 02 2006 15:04:05 -0700")
+	headers["Date"] = time.Now().In(location).Format("Mon, 02 Jan 2006 15:04:05 -0700")
 
 	body := ""
 	for k, v := range headers {
d1b0f5b5a63fa0a02e900b3a772ac2dbf759ed27
Tomas Machalek <tomas@machalek.xyz>
2024-02-02 08:06:24 +0100

    SessionValue - validator comparing is now part of the type

--- session/session.go
+++ session/session.go
@@ -1,13 +1,29 @@
 package session
 
-import "strings"
+import (
+	"crypto/sha256"
+	"crypto/subtle"
+	"fmt"
+	"strings"
+)
 
+// CNCSessionValue represents a parsed CNC session where
+// two values are used - one for fast session row selection
+// and the other one (Validator) for secure value comparison
 type CNCSessionValue struct {
 	Selector  string
 	Validator string
 }
 
-func (cv CNCSessionValue) UpdateFrom(v string) {
+// String returns a stardard value representation
+// as stored in CNC session database.
+func (cv CNCSessionValue) String() string {
+	return fmt.Sprintf("%s-%s", cv.Selector, cv.Validator)
+}
+
+// UpdateFrom sets `Selector` and `Validator` using parsed
+// values from a raw session value `v`
+func (cv *CNCSessionValue) UpdateFrom(v string) {
 	tmp := strings.SplitN(v, "-", 2)
 	if len(tmp) > 1 {
 		cv.Selector = tmp[0]
@@ -19,6 +35,24 @@ func (cv CNCSessionValue) UpdateFrom(v string) {
 	}
 }
 
+// IsZero tests for empty value (both `Selector` and `Validator` must be empty)
 func (cv CNCSessionValue) IsZero() bool {
 	return cv.Selector == "" && cv.Validator == ""
+}
+
+// CompareWithHash compares `Validator` with provided hash `h`.
+// (the `Validator` is hashed with sha256 before comparing and
+// the comparing function works in constant time no matter how
+// long common prefix the compared values have (but that works
+// only for values of the same size))
+func (cv CNCSessionValue) CompareWithHash(h string) (bool, error) {
+	hasher := sha256.New()
+	if _, err := hasher.Write([]byte(cv.Validator)); err != nil {
+		return false, err
+	}
+	hashedValidator := fmt.Sprintf("%x", hasher.Sum(nil))
+	if subtle.ConstantTimeCompare([]byte(hashedValidator), []byte(h)) == 1 {
+		return true, nil
+	}
+	return false, nil
 }

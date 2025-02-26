dc6c2c0f827567fd87a3f7764f3c262c37525fc0
Tomas Machalek <tomas@machalek.xyz>
2025-02-26 11:48:47 +0100

    1) add a concept of proxy response interceptor, ...
    2) create a specific interceptor for Hex to handle
    "not found" infinite redirect

--- services/backend/hex/conf.go
+++ services/backend/hex/conf.go
@@ -6,6 +6,52 @@
 
 package hex
 
-import "apiguard/services/cnc"
+import (
+	"apiguard/proxy"
+	"apiguard/services/cnc"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/rs/zerolog/log"
+)
 
 type Conf = cnc.ProxyConf
+
+// Interceptor function is used to handle specific Hex behavior
+// in case a provided word is not found. In such case, a simple
+// http client may end up in an infinite loop (APIGuard by default
+// would end up with status 429 (too many requests)).
+// The function searches for specific Location in headers where the
+// URL contains a message that the word was not found. In such case
+// we rewrite the response to provide an empty result.
+func Interceptor(pr *proxy.ProxiedResponse) {
+	// we must find and handle the following redirect:
+	// index.php?msg=Nenalezeny žádné výsledky pro dotaz 'rouhat'
+	// ... to prevent infinite redirect loop and make sure something
+	// valid is returned.
+
+	location := pr.Headers.Get("location")
+	if location != "" {
+		locUrl, err := url.Parse(location)
+		if err != nil {
+			log.Error().
+				Err(err).
+				Msg("failed to parse header Location from Hex backend, ignoring")
+			return
+		}
+		msg := locUrl.Query().Get("msg")
+		if strings.HasPrefix(strings.ToLower(msg), "nenalezeny") {
+			fakePage := []byte("<html><body><script>\n" +
+				"var hex = " +
+				"{\"size\": {}, \"count\": 0, \"countY\": {}, \"table\": [], \"sorting\": {}};\n" +
+				"</script></body></html>\n")
+			pr.StatusCode = http.StatusOK
+			pr.Headers.Del("location")
+			pr.Headers.Del("content-encoding")
+			pr.Headers.Set("content-length", fmt.Sprintf("%d", len(fakePage)))
+			pr.Body = fakePage
+		}
+	}
+}

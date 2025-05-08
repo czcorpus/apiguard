16398bc99ebe4969d24de16c634cb77c6395bdf7
Tomas Machalek <tomas@machalek.xyz>
2025-05-08 17:40:43 +0200

    Fix streaming cache - must ignore ping records

--- wagstream/cache.go
+++ wagstream/cache.go
@@ -11,6 +11,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bytedance/sonic"
@@ -104,6 +105,10 @@ type PersistentCache struct {
 	buffer        writeChunksReqs
 }
 
+func (backend *PersistentCache) isPingEntry(ch CacheWriteChunkReq) bool {
+	return strings.HasPrefix(string(ch.Data), "data: ")
+}
+
 func (backend *PersistentCache) listenAndWriteAccesses(ctx context.Context) {
 	for {
 		select {
@@ -124,6 +129,10 @@ func (backend *PersistentCache) listenAndWriteAccesses(ctx context.Context) {
 			}
 		case entry := <-backend.writes:
 			// TODO remove old recs
+			if backend.isPingEntry(entry) {
+				log.Debug().Msgf("cache skipping PING entry: %s", string(entry.Data))
+				continue
+			}
 			mergedEntry := backend.buffer.appendToExisting(entry)
 			if mergedEntry.Flush {
 				firstErrMsg, event, err := findErrorMsgInStream(mergedEntry.Data)

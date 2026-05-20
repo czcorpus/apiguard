9bba588bfe7665e5f6562774a165153ae5559540
Martin Zimandl <martin.zimandl@gmail.com>
2026-05-20 17:26:44 +0200

    ElasticOutputRecord

---
+++ globctx/elastic.go
@@ -0,0 +1,127 @@
+// Copyright 2022 Tomas Machalek <tomas.machalek@gmail.com>
+// Copyright 2022 Martin Zimandl <martin.zimandl@gmail.com>
+// Copyright 2022 Department of Linguistics,
+//                Faculty of Arts, Charles University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package globctx
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/czcorpus/apiguard/common"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+	lua "github.com/yuin/gopher-lua"
+)
+
+type ElasticOutputRecord struct {
+	Service      string         `json:"service"`
+	ActionType   string         `json:"actionType"`
+	ProcTime     float64        `json:"procTime"`
+	IsCached     bool           `json:"isCached"`
+	IndirectCall bool           `json:"indirectCall"`
+	UserID       common.UserID  `json:"userId,omitempty"`
+	IPAddress    string         `json:"ipAddress,omitempty"`
+	UserAgent    string         `json:"userAgent,omitempty"`
+	RequestPath  string         `json:"requestPath,omitempty"`
+	Args         map[string]any `json:"args,omitempty"`
+	CountryName  string         `json:"countryName,omitempty"`
+	Latitude     float32        `json:"latitude,omitempty"`
+	Longitude    float32        `json:"longitude,omitempty"`
+	Timezone     string         `json:"timezone,omitempty"`
+	Time         time.Time      `json:"time"`
+}
+
+// SetLocation sets the geographical location data for the record.
+func (r *ElasticOutputRecord) SetLocation(countryName string, latitude float32, longitude float32, timezone string) {
+	r.CountryName = countryName
+	r.Latitude = latitude
+	r.Longitude = longitude
+	r.Timezone = timezone
+}
+
+// ToJSON creates an object suitable for storing to ElasticSearch, CouchDB and other
+// document-oriented databases.
+func (r *ElasticOutputRecord) ToJSON() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+// GetID creates an idempotent unique identifier of the record.
+// This can be typically accomplished by hashing the original log record.
+// This generates a random UUID-based ID for uniqueness.
+func (r *ElasticOutputRecord) GetID() string {
+	id := uuid.New()
+	sum := sha1.New()
+	_, err := sum.Write([]byte(id.String()))
+	if err != nil {
+		log.Error().Err(err).Msg("problem generating hash")
+		return ""
+	}
+	return hex.EncodeToString(sum.Sum(nil))
+}
+
+// GenerateDeterministicID generates the same ID for the same record properties.
+// This is essential for easier update in the Elasticsearch archive.
+func (r *ElasticOutputRecord) GenerateDeterministicID() string {
+	// Create a deterministic hash from all record fields
+	data := fmt.Sprintf("%s|%s|%f|%v|%v|%s|%f|%f|%s|%s|%s",
+		r.Service,
+		r.ActionType,
+		r.ProcTime,
+		r.IsCached,
+		r.IndirectCall,
+		r.IPAddress,
+		r.Latitude,
+		r.Longitude,
+		r.Timezone,
+		r.CountryName,
+		r.Time.String(),
+	)
+
+	sum := sha1.New()
+	_, err := sum.Write([]byte(data))
+	if err != nil {
+		log.Error().Err(err).Msg("problem generating deterministic hash")
+		return ""
+	}
+	return hex.EncodeToString(sum.Sum(nil))
+}
+
+// GetType returns the app type as defined by an external convention.
+// Examples: kontext, syd, morfio, treq, apiguard, apiguard-kontext, etc.
+func (r *ElasticOutputRecord) GetType() string {
+	if r.Service != "" {
+		return r.Service
+	}
+	return "apiguard"
+}
+
+// GetTime returns the time of the log record.
+func (r *ElasticOutputRecord) GetTime() time.Time {
+	return r.Time
+}
+
+// SetTime sets the time of the log record (used for Lua scripting).
+func (r *ElasticOutputRecord) SetTime(t time.Time) {
+	r.Time = t
+}
+
+func (r *ElasticOutputRecord) LSetProperty(name string, value lua.LValue) error {
+	return fmt.Errorf("scripting not supported")
+}
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/jackc/pgx/v5 v5.8.0
 	github.com/rs/zerolog v1.34.0
 	github.com/stretchr/testify v1.11.1
+	github.com/yuin/gopher-lua v1.1.1
 	golang.org/x/sync v0.20.0
 	golang.org/x/time v0.15.0
 )
@@ -54,7 +55,6 @@ require (
 	github.com/quic-go/quic-go v0.59.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
-	github.com/yuin/gopher-lua v1.1.1 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
 	golang.org/x/arch v0.22.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
